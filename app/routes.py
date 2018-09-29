2a4add6801991c225ededbf1ebd7877ff97714e9
Matthias Als <matt035343@gmail.com>
2018-09-29 18:28:09 +0000

    Added POST routes for interacting with chromecast

--- app/routes.py
+++ app/routes.py
@@ -2,11 +2,70 @@ from flask import render_template,request
 from app import app,db
 from app.models import Satellite
 import json
+import pychromecast
+from pychromecast.controllers.youtube import YouTubeController
+import spotify_token
+import spotipy
+import spotipy.util as util
+
+CHROMECASTS = pychromecast.get_chromecasts()
+print("Done loading CCs")
+CC_NAME = "TT"
+
 
 @app.route("/")
 def hello():
     return ("Hello world")
 
+
+@app.route("/music/play", methods=['POST'])
+def play():
+    if not ccReady():
+        return "Chromecast not found"
+
+    mc, _ = getMediaController()
+    
+    json = request.json
+    
+    if 'url' not in json or not json['url'].endswith(".mp3"):
+        return "You must provide url to an mp3 file"
+
+    time = 0
+    if 'time' in json:
+        time = json['time']
+
+    # current time must be set to be able to seek music later
+    mc.play_media(json['url'], 'audio/mp3', current_time = time)
+    
+    return "success"
+
+@app.route("/music/update", methods=['POST'])
+def update():
+    if not ccReady():
+        return "Chromecast not found"
+
+    mc, cast = getMediaController()
+
+    json = request.json
+
+    if 'time' in json:
+        mc.seek(json['time'])
+
+    if 'volume' in json:
+        cast.set_volume(json['volume'])
+
+    return "success"
+
+@app.route("/music/pause", methods=['POST'])
+def pause():
+    if not ccReady():
+        return ("Chromecast not found")
+
+    mc, _ = getMediaController()
+
+    mc.pause()
+
+    return "success"
 
 @app.route("/home/<string:name>", methods=['POST', 'GET'])
 def home(name):
@@ -44,3 +103,16 @@ def connectPOST(request):
     return json.dumps(request.json)
 def connectGET(request):
     return request.method
+
+def ccReady():
+    if len(CHROMECASTS) == 0:
+        return False
+    casts = [cc for cc in CHROMECASTS if cc.device.friendly_name == CC_NAME]
+    if len(casts) == 0:
+        return False
+    return True
+
+def getMediaController():
+    cast = next(cc for cc in CHROMECASTS if cc.device.friendly_name == CC_NAME)
+    cast.wait()
+    return cast.media_controller, cast
